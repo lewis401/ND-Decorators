9cf17d7c18c5162f5372100b84b8df340322bf59
lewis401 <lewis-murphy@hotmail.com>
2025-08-11 13:55:20 +0100

    Delete src directory

--- src/api/base44Client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@base44/sdk';
-// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
-
-// Create a client with authentication required
-export const base44 = createClient({
-  appId: "6894a2c4de02ff41bc95ec42", 
-  requiresAuth: true // Ensure authentication is required for all operations
-});
